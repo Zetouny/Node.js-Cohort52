a6ed19c413b60ccaee2c2970eaa788806bb9a6d0
A. Zetouny <Zet_Ahmad@hotmail.com>
2025-05-08 19:03:08 +0200

    Prep-Excersice Week 3 Completed

--- week3/prep-exercise/server/app.js
+++ week3/prep-exercise/server/app.js
@@ -1,15 +1,21 @@
-import express from 'express';
-// TODO Use below import statement for importing middlewares from users.js for your routes
-// TODO import { ....... } from "./users.js";
+import express from "express";
+import { register, login, profile, logout } from "./users.js";
 
 let app = express();
 
 app.use(express.json());
-// TODO: Create routes here, e.g. app.post("/register", .......)
+
+app.post("/auth/register", register);
+
+app.post("/auth/login", login);
+
+app.get("/auth/profile", profile);
+
+app.post("/auth/logout", logout);
 
 // Serve the front-end application from the `client` folder
-app.use(express.static('client'));
+app.use(express.static("client"));
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
--- week3/prep-exercise/server/users.js
+++ week3/prep-exercise/server/users.js
@@ -1,12 +1,84 @@
-import newDatabase from './database.js'
+import newDatabase from "./database.js";
+import { hash, compare } from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
 
 // Change this boolean to true if you wish to keep your
 // users between restart of your application
-const isPersistent = false
-const database = newDatabase({isPersistent})
+const isPersistent = true;
+const database = newDatabase({ isPersistent });
+const SECRET = "H6AIgu0wsGCH2mC6ypyRubiPoPSpV4t1";
 
-// Create middlewares required for routes defined in app.js
-// export const register = async (req, res) => {};
+export const register = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400);
+    res.json({ message: "Username and Password are required" });
+    return;
+  }
 
-// You can also create helper functions in this file to help you implement logic
-// inside middlewares
+  const saltRounds = 12;
+  const hashedPassword = await hash(password, saltRounds);
+  const createdUser = database.create({
+    username: username,
+    password: hashedPassword,
+  });
+
+  res.status(200);
+  res.json({ id: createdUser.id, username: createdUser.username });
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400);
+    res.json({ message: "Username and Password are required" });
+    return;
+  }
+
+  const getUser = database.getByUsername(username);
+  const usersPassword = getUser.password;
+  const comparedPassword = await compare(password, usersPassword);
+
+  if (!comparedPassword) {
+    res.status(400);
+    res.json({ message: "Password is incorrect" });
+    return;
+  }
+
+  const usersID = getUser.id;
+  const token = jsonwebtoken.sign(usersID, SECRET);
+
+  res.status(201);
+  res.json({ token: token });
+};
+
+export const profile = async (req, res) => {
+  const authorizationHeader = req.headers["authorization"];
+  if (!authorizationHeader) {
+    res.status(400);
+    res.json({ message: "Token not found" });
+    return;
+  }
+  const token = authorizationHeader.replace("Bearer ", "");
+
+  try {
+    const verifiedUser = jsonwebtoken.verify(token.trim(), SECRET);
+    if (!verifiedUser) {
+      throw Error("Oops");
+    }
+
+    const userData = database.getById(verifiedUser);
+
+    res.status(201);
+    res.json({ message: `Welcome! ${userData.username}` });
+  } catch (error) {
+    res.status(400);
+    res.json({ message: "Invalid token" });
+    return;
+  }
+};
+
+export const logout = async (req, res) => {
+  res.status(204);
+  res.json({ message: "Logged out successfully!" });
+};
